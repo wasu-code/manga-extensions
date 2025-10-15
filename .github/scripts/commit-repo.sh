cbfdfdf93c268797222b877fb386b089775acfcb
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-10-15 14:24:49 +0200

    debug

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md --exclude repo.json ../main/repo/ .
-ls -l ../main/repo/index.json
 cat ../main/repo/index.json
-ls -l ./index.json
+cat ./index.json
+rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md --exclude repo.json ../main/repo/ .
+cat ../main/repo/index.json
 cat ./index.json
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
