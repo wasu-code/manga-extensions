16694e449dd0da3cff4501b2c359c3b66dc6ebf1
wasu-code <61418403+wasu-code@users.noreply.github.com>
2026-03-06 11:26:21 +0100

    fix(utils): log() extension function not showing caller correctly

--- utils/src/kuchihige/utils/Debug.kt
+++ utils/src/kuchihige/utils/Debug.kt
@@ -7,8 +7,8 @@ package kuchihige.utils
  */
 fun <T> T.log(tag: String = "Kuchihige"): T {
     val stack = Throwable().stackTrace
-    // stack[0] is this line, so stack[1] is the caller
-    val caller = stack.getOrNull(1)
+    // stack[0] is this line, so stack[1] is the T.log(...), stack[2] is the caller
+    val caller = stack.getOrNull(2)
 
     val location = if (caller != null) {
         "${caller.fileName}:${caller.lineNumber}"
@@ -23,17 +23,14 @@ fun <T> T.log(tag: String = "Kuchihige"): T {
     } ?: "null"
 
     val message = """
-        ┌─ 𝕃𝕆𝔾
-        │ $location
-        │ 𝕞:  $method
-        │ 𝕥:  $thread
-        │
-        │ 𝕋:  $type
-        │────────────────────────────
-        │ 𝕧:  $this
-        └────────────────────────────
+        𝕧:  $this
+            → $location
+          𝕞:  $method
+          𝕥:  $thread
+          𝕋:  $type
     """.trimIndent()
 
     android.util.Log.d(tag, message)
+
     return this
 }
